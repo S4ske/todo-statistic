966595bfeb3db30fd733f7c3287a8860fe1984a7
Shutcing <maksim19111@gmail.com>
2025-03-04 18:11:43 +0500

    first commit

--- index.js
+++ index.js
@@ -1,25 +1,59 @@
-const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
-const {readLine} = require('./console');
+const { getAllFilePathsWithExtension, readFile } = require("./fileSystem");
+const { readLine } = require("./console");
 
 const files = getFiles();
 
-console.log('Please, write your command!');
+console.log("Please, write your command!");
 readLine(processCommand);
 
 function getFiles() {
-    const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
-    return filePaths.map(path => readFile(path));
+  const filePaths = getAllFilePathsWithExtension(process.cwd(), "js");
+  return filePaths.map((path) => readFile(path));
+}
+
+function isGood(str) {
+  const splitedStr = str.split("TODO")[0];
+  if (
+    splitedStr.split("").filter((x) => x == "'").length % 2 == 0 &&
+    splitedStr.split("").filter((x) => x == '"').length % 2 == 0 &&
+    splitedStr.split("").filter((x) => x == "`").length % 2 == 0
+  ) {
+    return true;
+  }
+
+  return false;
+}
+
+function getTodos() {
+  const files = getFiles();
+  const todos = [];
+  for (const file of files) {
+    let lines = file.split("\n");
+    for (const line of lines) {
+      if (line.includes("// TODO") && isGood(line)) {
+        todos.push(`//${line.split("//")[1]}`);
+      }
+    }
+  }
+
+  return todos;
 }
 
 function processCommand(command) {
-    switch (command) {
-        case 'exit':
-            process.exit(0);
-            break;
-        default:
-            console.log('wrong command');
-            break;
-    }
+  switch (command) {
+    case "exit":
+      process.exit(0);
+      break;
+    case "show":
+      const todos = getTodos();
+      for (const todo of todos) {
+        console.log(todo);
+      }
+      break;
+    default:
+      console.log("wrong command");
+      break;
+  }
 }
 
 // TODO you can do it!
