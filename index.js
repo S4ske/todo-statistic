a23d9b2513e3c6c5056ff05384f6e0d53cccc6e3
S4ske <sany1111.ru@mail.ru>
2025-03-04 18:39:11 +0500

    Сделал ещё

--- index.js
+++ index.js
@@ -39,8 +39,26 @@ function getTodos() {
   return todos;
 }
 
+function checkRe(str) {
+  const regex = /\/\/ TODO\s+([^;]+);\s+([^;]+);\s+(.+)/;
+
+  const match = str.match(regex);
+  
+  if (match) {
+    const author = match[1].trim();
+    const date = match[2].trim();
+    const comment = match[3].trim();
+    return { author: author, date: date, comment: comment, raw: str }
+  }
+  else {
+    return false;
+  }
+  
+}
+
 function processCommand(command) {
-  switch (command) {
+  const splitedCommand = command.split(' ');
+  switch (splitedCommand[0]) {
     case "important":
       const importantTodos = getTodos().filter((x) => x.includes("!"));
       for (const todo of importantTodos) {
@@ -54,6 +72,14 @@ function processCommand(command) {
       const todos = getTodos();
       for (const todo of todos) {
         console.log(todo);
+      }
+      break;
+    case "user":
+      const reTodos = getTodos().map(checkRe);
+      for (const todo of reTodos) {
+        if (todo && todo.author.toLowerCase() === splitedCommand[1].toLowerCase()) {
+          console.log(todo.raw);
+        }
       }
       break;
     default:
